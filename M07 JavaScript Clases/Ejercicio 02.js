60b37492055018818839e2de119d1678f8d3c68e
Jhonatan Vasquez Reyna <jhona141200@gmail.com>
2023-08-10 23:56:10 -0500

    m7 solved

--- M07 JavaScript Clases/Ejercicio 02.js
+++ M07 JavaScript Clases/Ejercicio 02.js
@@ -36,6 +36,7 @@ function agregarMetodo() {
    // Este método toma la propiedad "nombre" y "edad", y devuelve el string:
    // Ejemplo: "Juan, 22 años".
    // Tu código:
+   // Solucion
    Persona.prototype.datos = function () {
       return `${this.nombre}, ${this.edad} años`
    };
